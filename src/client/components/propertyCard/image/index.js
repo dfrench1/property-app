60f802138d4d8c1eb7a8073ede9f1f19e5cca4c0
dfrench1 <dfrench1090@gmail.com>
2019-03-12 21:36:24 +0000

    build error

--- src/client/components/propertyCard/image/index.js
+++ src/client/components/propertyCard/image/index.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FavBtn from '../../favouritesBtn'
-import {ImgStyle} from './styled'
+import React from "react";
+import PropTypes from "prop-types";
+import FavBtn from "../../favouritesBtn";
+import { ImgStyle } from "./styled";
 import FadeIn from "react-lazyload-fadein";
 
-const Image = ({data}) => {
-    return (
-      <FadeIn 
-        height={400} 
-        duration={100}
-      >
-        {onload => (
+const Image = ({ data }) => {
+  return (
+    <FadeIn height={400} duration={100}>
+      {onload => (
         <React.Fragment>
           <ImgStyle
             key={data._id}
-            src={`https://s3-eu-west-1.amazonaws.com/airbnb-assets/${data.images[0]}`}
-           
+            // src={`https://s3-eu-west-1.amazonaws.com/airbnb-assets/${
+            //   data.images[0]
+            // }`}
             onLoad={onload}
           />
           <FavBtn data={data} />
-         </React.Fragment>
-        )}
-      </FadeIn>
-    );
+        </React.Fragment>
+      )}
+    </FadeIn>
+  );
 };
 
-Image.propTypes = {
-    
-};
+Image.propTypes = {};
 
 export default Image;
