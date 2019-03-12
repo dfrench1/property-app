641e998094ca3ddf940fd646ad7ed56f45dc1be3
dfrench1 <dfrench1090@gmail.com>
2019-03-12 20:32:14 +0000

    build error

--- src/client/functions/AddFavourite.js
+++ src/client/functions/AddFavourite.js
@@ -1,6 +1,6 @@
 export function addFavourite(data, setFave, user){
     data.user = user
-    fetch(`${process.env.API}/favourites`, {
+    fetch(`${process.env.API}/api/favourites`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers:{
