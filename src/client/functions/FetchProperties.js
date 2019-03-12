ee6492275b2db179a3bf9dcb56b4528f1ffc382a
dfrench1 <dfrench1090@gmail.com>
2019-03-12 00:13:43 +0000

    build error

--- src/client/functions/FetchProperties.js
+++ src/client/functions/FetchProperties.js
@@ -1,5 +1,5 @@
 export function fetchProperties(setData){
-    fetch(`${process.env.API}/properties`)
+    fetch(process.env.API + "properties")
     .then(res => res.json())
     .then(json => setData(json))
     .catch(err => console.log(err));
